73360b594f6420634fc14d8bd7edf98788c90e8d
Camilo Montt Fierro <55846057+cmonttf@users.noreply.github.com>
2022-10-17 09:59:35 -0300

    cambio de nombre y se agrega los randomicos

--- hotmigas.py
+++ hotmigas.py
@@ -3,3 +3,16 @@ import pandas as pd
 import sys
 import time
 
+#Función randomica entre 0 y 1
+def rand_bin():
+    valor = np.random.randint(0, 1)
+    return valor
+
+
+#Función randomico entre 1 y n
+def rand_n(n):
+    valor = np.random.randint(0,n)
+    return valor
+
+print(rand_bin())
+print(rand_n(15))
