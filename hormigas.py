5a61a436a649478fc4e1fa60ad9b477cfcb53c42
Camilo Montt Fierro <55846057+cmonttf@users.noreply.github.com>
2022-10-17 09:59:54 -0300

    cambio de nombre

--- hormigas.py
+++ hormigas.py
@@ -5,7 +5,7 @@ import time
 
 #Función randomica entre 0 y 1
 def rand_bin():
-    valor = np.random.randint(0, 1)
+    valor = np.random.random_sample()
     return valor
 
 
@@ -13,6 +13,8 @@ def rand_bin():
 def rand_n(n):
     valor = np.random.randint(0,n)
     return valor
+
+
 
 print(rand_bin())
 print(rand_n(15))
