092d1cfae941294f44bab87ddaf053a4d60cce75
Camilo Montt Fierro <55846057+cmonttf@users.noreply.github.com>
2022-10-24 10:42:36 -0300

    actualizado

--- hormigas.py
+++ hormigas.py
@@ -20,6 +20,24 @@ def solucionCalculaCosto(n, s, c):
         aux += c[s[i]][s[i+1]]
     return aux
 
+def inicializarCoordenadas(entrada):
+    matrizCoordenadas = pd.read_table(entrada, header=None, delim_whitespace=True, skiprows=6, skipfooter=2)
+    matrizCoordenadas = matrizCoordenadas.drop(columns=0,axis=1).to_numpy()
+    return matrizCoordenadas
+
+def inicializarDistancia(numVariables, matrizCoordenadas):
+    matrizDistancia = np.full((numVariables,numVariables), fill_value=-1.0,dtype=float)
+    for i in range(numVariables - 1):
+        for j in range(i + 1, numVariables):
+            matrizDistancia[i][j] = np.sqrt(np.sum(np.square(matrizCoordenadas[i]-matrizCoordenadas[j])))
+            matrizDistancia[j][i] = matrizDistancia[i][j]
+    return matrizDistancia
+
+def inicializarHeuristica(matrizDistancia):
+    matrizHeuristica = np.full_like(matrizDistancia, fill_value=1/matrizDistancia,dtype=float)
+    np.fill_diagonal(matrizHeuristica,0)
+    return matrizHeuristica
+
 sys.argv = ['hormigas.py', '4', '40', '200', '0.1', '2.5', '0.9', 'berlin52.tsp']
 
 if len(sys.argv) == 8:
@@ -38,22 +56,16 @@ else:
 tiempo_ini = time.process_time()
 np.random.seed(semilla)
 
-matrizCoordenadas = pd.read_table(entrada, header=None, delim_whitespace=True, skiprows=6, skipfooter=2)
-matrizCoordenadas = matrizCoordenadas.drop(columns=0,axis=1).to_numpy()
-print('matrizCoordenadas-shape:', matrizCoordenadas.shape)
+matrizCoordenadas = inicializarCoordenadas(entrada)
+#print('matrizCoordenadas-shape:', matrizCoordenadas.shape)
 numVariables = matrizCoordenadas.shape[0]
-print('Matriz de Coordenadas:\n', matrizCoordenadas, '\ntamaño:',matrizCoordenadas.shape,'\ntipo:', type(matrizCoordenadas))
-print('Número de variables:',numVariables)
+#print('Matriz de Coordenadas:\n', matrizCoordenadas, '\ntamaño:',matrizCoordenadas.shape,'\ntipo:', type(matrizCoordenadas))
+#print('Número de variables:',numVariables)
 
-matrizDistancia = np.full((numVariables,numVariables), fill_value=-1.0,dtype=float)
-for i in range(numVariables - 1):
-    for j in range(i + 1, numVariables):
-        matrizDistancia[i][j] = np.sqrt(np.sum(np.square(matrizCoordenadas[i]-matrizCoordenadas[j])))
-        matrizDistancia[j][i] = matrizDistancia[i][j]
-print('Matriz de Distancia:\n', matrizDistancia, '\ntamaño:', matrizDistancia.shape, '\tipo:', type(matrizDistancia))
+matrizDistancia = inicializarDistancia(numVariables, matrizCoordenadas)
+#print('Matriz de Distancia:\n', matrizDistancia, '\ntamaño:', matrizDistancia.shape, '\tipo:', type(matrizDistancia))
 
-matrizHeuristica = np.full_like(matrizDistancia, fill_value=1/matrizDistancia,dtype=float)
-np.fill_diagonal(matrizHeuristica,0)
+matrizHeuristica = inicializarHeuristica(matrizDistancia)
 print('Matriz de Heuristica:\n', matrizHeuristica, '\ntamaño:', matrizHeuristica.shape, '\ntipo:', type(matrizHeuristica))
 
 solucionMejor = np.zeros(numVariables)
@@ -68,3 +80,11 @@ print('Iteración donde se encontró la mejor solución:', solucionMejorIteracio
 Tij0 = 1/(numVariables*solucionMejorCosto)
 matrizFeromona = np.full_like(matrizDistancia,fill_value=Tij0,dtype=float)
 print('Matriz de Feromona:\n',matrizFeromona, '\ntamaño:', matrizFeromona.shape, '\ntipo:', type(matrizFeromona))
+
+while solucionMejorIteracion < ite:
+    #for i in range()
+    #print(solucionMejorIteracion)
+    solucionMejorIteracion+=1
+
+tiempo_fin = time.process_time()
+print('Tiempo:',(tiempo_fin-tiempo_ini),'seg.\n')
